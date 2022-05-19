e602a04ab475eefe86625b001a15ee59f93adf52
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-05-19 16:26:14 -0600

    At this stage I added new component About section with instructions

---
+++ src/Components/About.js
@@ -0,0 +1,13 @@
+function About() {
+
+    return (
+        <div>
+            <h2>Instructions</h2>
+          <p>Select character of choice. <br /> You will see more information for the character you selected. <br /> If you like the character you selected you can now add it to your favorites. 
+          <br /> Navigate to favorites and see you favorite characters! <br/> If you do not like your favorite characters anymore, click delete button and it will not longer be in your favorites.</p>
+        </div>
+    )
+
+
+}
+export default About
