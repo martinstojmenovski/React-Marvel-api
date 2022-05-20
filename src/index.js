92e48bacb44060e7be80be9002a46fa122f24bd9
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-05-20 15:37:26 -0600

    new try to deploy my page

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter as Router} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
